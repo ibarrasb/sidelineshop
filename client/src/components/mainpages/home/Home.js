d6941de4425ef714c8016f07ae0c2c7a1fdc9b35
Eduardo Ibarra <eddieibarra43@gmail.com>
2024-03-18 01:24:33 -0500

    heroku

---
+++ client/src/components/mainpages/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './home.css'
+
+function Home() {
+    return (
+        <div>
+            <h1 className="intro">Welcome to Sideline</h1>
+            <div className="container">
+            <p1 className="stuff">The go to offical place to shop for any sports product! Every item is guaranteed authentic, and for the best price you can find anywhere.</p1>
+            </div>
+            <div className="container">
+            <p1 className="stuff">Login or create a new account to shop your favorite team or players items before they run out!</p1>
+            </div>
+            </div>
+    );
+}
+
+export default Home;
---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+// Routes
+app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
+app.use('/api', require('./routes/paymentRouter'))
+
+
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URL;
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+// Build for Heroku
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server is running on port..', PORT);
+});
+
+connectToMongo();
